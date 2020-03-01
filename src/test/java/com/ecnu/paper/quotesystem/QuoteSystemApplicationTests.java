eee83e06b8261b4377ff12e09dffcc3bf09414a5
Shuiashuai Liu <924829576@qq.com>
2020-03-01 22:58:44 +0800

    first commit

---
+++ src/test/java/com/ecnu/paper/quotesystem/QuoteSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecnu.paper.quotesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuoteSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
